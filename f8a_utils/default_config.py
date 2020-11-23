58841fa466e31eb1a9d74807061aa455f59fac98
Deepak Sharma <deepshar@redhat.com>
2020-11-23 18:33:12 +0530

    test

--- f8a_utils/default_config.py
+++ f8a_utils/default_config.py
@@ -3,4 +3,4 @@ import os
 
 SNYK_API_TOKEN_VALIDATION_URL = os.getenv('SNYK_API_TOKEN_VALIDATION_URL',
                                           'https://snyk.io/api/v1/verify/token')
-ENCRYPTION_KEY_FOR_SNYK_TOKEN = os.getenv('ENCRYPTION_KEY_FOR_SNYK_TOKEN', 'SNYK')
+ENCRYPTION_KEY_FOR_SNYK_TOKEN = 'UMF9rhYIcaUKRdWfcAoUK1FQCXi_ENURjX1bGDgkljA='
